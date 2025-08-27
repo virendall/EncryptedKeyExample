e5ebb023c22761b84c36f8542e583ad172e4d8dc
Virender <viru.dall@gmail.com>
2025-08-27 19:16:44 +0530

    Reflection added

--- Generated/ProtectedKeys.swift
+++ Generated/ProtectedKeys.swift
@@ -1,7 +1,11 @@
 // Auto-generated file — DO NOT EDIT
 import Foundation
 
-struct ProtectedKeys {
+struct ProtectedKeys: CustomReflectable {
     static let encryptedAPIKey: [UInt8] = [113, 3, 97, 29, 255, 52, 91, 40, 79, 213, 243, 126, 134, 25, 34, 185, 100, 43, 72, 219, 113, 29, 204, 19, 206, 45, 224, 122, 159, 79, 64, 185, 158, 133, 10, 133, 45, 242, 188, 8, 29, 130, 186, 240, 246, 199, 219, 80, 194, 213, 254]
     static let aesKey: [UInt8] = [225, 84, 165, 178, 112, 169, 231, 103, 61, 246, 66, 104, 113, 214, 90, 133, 100, 35, 58, 24, 247, 229, 234, 9, 91, 85, 251, 246, 171, 118, 8, 163]
+    
+    var customMirror: Mirror {
+        return Mirror(self, children: [])
+    }
 }
